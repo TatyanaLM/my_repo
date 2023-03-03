e79e389ba9c6fdea46ab84860a747aca4c8477da
Latukhina_T <latukhinatm@gmail.com>
2023-03-03 21:31:06 +0300

    1 task

---
+++ task2.py
@@ -0,0 +1,11 @@
+var1 = float(input("Press first number "))
+var2 = float(input("Press second number "))
+
+if var1 == var2:
+    print('Числа равны!')
+elif var1 > var2:
+    print(var2)
+    print(var1)
+else:
+    print(var1)
+    print(var2)
