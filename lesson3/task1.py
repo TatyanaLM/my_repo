f357cbcdcda855bd3c559c0b220f579094cf3a35
Latukhina_T <latukhinatm@gmail.com>
2023-03-07 21:00:31 +0300

    task a b

---
+++ lesson3/task1.py
@@ -0,0 +1,12 @@
+#task a
+print('Задачка 1')
+numbers = [1, 23, 56, 21, 56]
+for i in range(len(numbers)):
+    print(numbers[i]**2)
+
+#task b
+print('Задачка 2')
+numbers = [45, 67, 34, 98, 12]
+for i in range(len(numbers)):
+    if numbers[i] % 2 == 0:
+        print(numbers[i])
