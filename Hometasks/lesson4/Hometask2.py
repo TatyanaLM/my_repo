96cea4dec3a33a422e9616841d1035f8103dcbd9
Latukhina_T <latukhinatm@gmail.com>
2023-03-12 19:24:12 +0300

    lesson 4 hometask 2 fix

--- Hometasks/lesson4/Hometask2.py
+++ Hometasks/lesson4/Hometask2.py
@@ -2,9 +2,13 @@ n = int(input("Введите размер вклада: "))
 m = int(input("Введите срок вклада в годах: "))
 
 def deposit (n, m):
-    rate = 0.08
+    rate = 0.06
     for i in range(m):
       n = n + n * rate
     return n
-
-print(f"Сумма вклада через {m} лет составит {deposit(n, m)}")
+if m == 1:
+    print(f"Сумма вклада через {m} год составит {deposit(n, m)}")
+elif 1 < m <= 4:
+    print(f"Сумма вклада через {m} года составит {deposit(n, m)}")
+else:
+    print(f"Сумма вклада через {m} лет составит {deposit(n, m)}")
