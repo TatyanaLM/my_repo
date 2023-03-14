eae3c0696e9ba35e22c0502be3a29a491495d940
Latukhina_T <latukhinatm@gmail.com>
2023-03-14 19:10:25 +0300

    lesson 4 hometask 4 fix

--- Hometasks/lesson4/Hometask4.py
+++ Hometasks/lesson4/Hometask4.py
@@ -1,4 +1,5 @@
-lst = [5854, 4, 55,718, 7, 81, 3, 659, 77, 765]
+lst = [5854, 4, 55, 718, 7, 81, 3, 659, 77, 765]
+#вариант 1
 def elements(lst):
     n = 0
     for i in range(1, len(lst)-1):
@@ -9,6 +10,7 @@ def elements(lst):
     return n
 print(elements(lst))
 
+#вариант 2
 def elements2(lst):
     n = 0
     for i in range(lst):
