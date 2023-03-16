72ef0135747298f32116bd54bfe6ef235b02239c
Latukhina_T <latukhinatm@gmail.com>
2023-03-17 00:09:10 +0300

    fix task 1 from lesson 5

--- Hometasks/lesson5/Hometask1.py
+++ Hometasks/lesson5/Hometask1.py
@@ -1,12 +1,11 @@
 
 from Hometask1_lesson5.utils import calculate_change
 
-price = float(input("Введите стоимость товара: "))
+price = input("Введите стоимость товара: ")
 paid = input("Введите купюры внесенные в кассу: ")
 
 change = calculate_change(price, paid)
 
-#print(f'Сдача: {change}')
 print(f'Купюры для сдачи: {change}')
-#print(f'Количество купюр/монет: {change}')
+
 
