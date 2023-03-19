d7e591f4707d1e6cdbf07bca53395aeb7e5114e5
Latukhina_T <latukhinatm@gmail.com>
2023-03-19 21:04:05 +0300

    hometask 4 lesson 6 fix

--- Hometasks/lesson6/Hometask4.py
+++ Hometasks/lesson6/Hometask4.py
@@ -7,3 +7,6 @@ x = 3
 elem = ['red', 523, 'jfgfg', 'Мира', 'Кирилл', '34', 734, 900]
 random_elem = [random.sample(elem, 3, counts=None) for i in range(x)]
 print(random_elem)
+
+random_list = random.sample([i for i in range(100)], random.randint(1, 10))
+print(random_list)
