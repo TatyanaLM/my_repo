58f2730c3d43eca69500b54acd07598c9b6824fd
Latukhina_T <latukhinatm@gmail.com>
2023-04-07 20:08:53 +0300

    Passport fix 2

--- lesson12/1.py
+++ lesson12/1.py
@@ -14,19 +14,30 @@ class Passport():
         print(f'Passport number: {self.number}.')
 
 class ForeignPassport(Passport):
-    def __init__(self, name, sname, date, country, number, visa):
+    def __init__(self, name, sname, date, country, number, visa, exp_date):
         super().__init__(name, sname, date, country, number)
         self.visa = visa
+        self.exp_date = exp_date
 
     def PrintInfo(self):
         super().PrintInfo()
         print(f'Visa: {self.visa}.')
+        print(f'Expiration date: {self.exp_date}.')
+
+    def days_before_expiration(self):
+        from datetime import datetime
+        start = datetime.today()
+        end = datetime.strptime(self.exp_date, "%d/%m/%y")
+        diff = (end - start)
+        print(f" Осталось дней: {diff}")
+    def next_visa(self):
+
 
 
 passport_1 = Passport("Tatyana", "Latukhina", "17.02", "Russia", 4509123456)
 #passport_1.PrintInfo()
 print("\n")
-foreignPassport_1 = ForeignPassport("Tatyana", "Latukhina", "17.02", "Russia", 4509123456, "Visa USA")
+foreignPassport_1 = ForeignPassport("Tatyana", "Latukhina", "17.02", "Russia", 4509123456, "Visa USA", "22/5/23")
 #foreignPassport_1.PrintInfo()
 passport_list = []
 passport_list.append(passport_1)
@@ -35,3 +46,6 @@ passport_list.append(foreignPassport_1)
 for i in passport_list:
     i.PrintInfo()
     print("\n")
+
+foreignPassport_1.days_before_expiration()
+
