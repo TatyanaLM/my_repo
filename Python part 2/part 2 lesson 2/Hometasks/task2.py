1bf627f5ce151042c36d117ca8bed27bfa062bac
Latukhina_T <latukhinatm@gmail.com>
2023-05-01 12:39:23 +0300

    Fix task 2

--- Python part 2/part 2 lesson 2/Hometasks/task2.py
+++ Python part 2/part 2 lesson 2/Hometasks/task2.py
@@ -3,37 +3,34 @@
 Строка содержит только цифры и операторы сложения, вычитания, умножения и деления.
 При этом использовать только базовые операции и функции."""
 
-def calculate(str):
-    list_str = list(str)
-    new_list = []
-    for current in list_str:
-        if current == "+" or current == "-" or current == "/" or current == "*":
-            new_list.append(current)
-        else:
-            new_list.append(int(current))
-    print(new_list)
-    for i in range(len(new_list)):
-        if new_list[i] == "*" or new_list[i] == "/":
-            if new_list[i] == "*":
-                num = new_list[i-1] * new_list[i+1]
-                new_list[i-1] = num
-                new_list[i+1] = "none"
-            else:
-                num = new_list[i-1] / new_list[i+1]
-                new_list[i-1] = float(num)
-                new_list[i+1] = "none"
-    print(new_list)
-    result = 0
-    for i in range(len(new_list)):
-        if new_list[i] == "+":
-            result = result + new_list[i-1] + new_list[i+1]
-        elif new_list[i] == "-":
-            result = result + new_list[i-1] - new_list[i+1]
-        else:
-            pass
-    return result
 
 
-assert calculate("2+3*4-5/2") == 10.5
+def calculate(expression):
+    stack = []
+    num = 0
+    sign = "+"
+    for i, char in enumerate(expression):
+        if char.isdigit():
+            num = num * 10 + float(char)
+        if (not char.isdigit() and not char.isspace()) or i == len(expression) - 1:
+            if sign == "+":
+                stack.append(num)
+            elif sign == "-":
+                stack.append(-num)
+            elif sign == "*":
+                stack.append(stack.pop() * num)
+            elif sign == "/":
+                stack.append(float(stack.pop() / num))
+            num = 0
+            sign = char
+    print(stack)
+    return sum(stack)
+
+print(calculate("2+3*4-5/2"))
+print(calculate("1+2+3+4+5"))
+print(calculate("10*5-7*3+2"))
+assert calculate("2+3*4-5/2") == 11.5
 assert calculate("1+2+3+4+5") == 15
-assert calculate("10*5-7*3+2") == 23
+assert calculate("10*5-7*3+2") == 31
+
+
