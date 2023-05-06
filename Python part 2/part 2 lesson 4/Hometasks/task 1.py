38b29a94dcb5d29c88c90a4c04592cd451935c00
Latukhina_T <latukhinatm@gmail.com>
2023-05-06 19:46:34 +0300

    task 1 fix

--- Python part 2/part 2 lesson 4/Hometasks/task 1.py
+++ Python part 2/part 2 lesson 4/Hometasks/task 1.py
@@ -5,29 +5,32 @@
 
 # импортируем класс
 class Student:
+    students = []
     def __init__(self, name, sname, course, score):
             self.name = name
             self.sname = sname
             self.course = course
             self.score = score
-            Student._students = []
 
+    @classmethod
     def add_student(cls, student):
         cls.students.append(student)
 
     def change_course(self, course):
         self.course = course
 
-    def print_students(self):
-            for student in self.students:
+    @classmethod
+    def print_students(cls):
+            for student in cls.students:
                 print(f'{student.name} {student.sname}, course: {student.course}, score: {student.score}')
 
-    def search_student(self, sname):
-            for student in self.students:
-                if student.sname == sname:
-                    print(f'{student.name} {student.sname}, course: {student.course}, score: {student.score}')
-            else:
-                print("Студент не найден!")
+    @classmethod
+    def search_student(cls, sname):
+        result = "Студент не найден!"
+        for student in cls.students:
+            if student.sname == sname:
+                result = f'{student.name} {student.sname}, course: {student.course}, score: {student.score}'
+        print(result)
 
 # создаем объекты студентов
 student1 = Student("Иван", "Иванов", 1, 4.5)
@@ -35,13 +38,15 @@ student2 = Student("Петр", "Петров", 2, 3.7)
 
 # добавляем студента
 student3 = Student("Сергей", "Сергеев", 1, 4.0)
+Student.add_student(student1)
+Student.add_student(student2)
 Student.add_student(student3)
 
 # изменяем данные студента
 student1.change_course(4)
 
 # выводим информацию о всех студентах
-student1.print_students()
+Student.print_students()
 
 # ищем студента по фамилии
-student1.search_student("Петрова")
+Student.search_student("Иванова")
